18d47619ee4bc7bd353a97c7112dcfcaa128d8ae
オシリス <rukiarayamashita@gmail.com>
2024-08-30 11:22:41 +0800

    Create pikachu.py

---
+++ pikachu.py
@@ -0,0 +1,103 @@
+import turtle as t
+
+def draw_pikachu():
+    t.bgcolor("white")
+    t.speed(9)
+    
+    # Ears
+    t.penup()
+    t.goto(-100, 150)
+    t.pendown()
+    t.begin_fill()
+    t.color("yellow")
+    t.circle(50, 180)
+    t.end_fill()
+
+    t.penup()
+    t.goto(100, 150)
+    t.pendown()
+    t.begin_fill()
+    t.circle(50, 180)
+    t.end_fill()
+
+    # Head
+    t.penup()
+    t.goto(-60, 100)
+    t.pendown()
+    t.begin_fill()
+    t.circle(120)
+    t.end_fill()
+
+    # Eyes
+    t.penup()
+    t.goto(-60, 100)
+    t.pendown()
+    t.begin_fill()
+    t.color("white")
+    t.circle(25)
+    t.end_fill()
+
+    t.penup()
+    t.goto(60, 100)
+    t.pendown()
+    t.begin_fill()
+    t.circle(25)
+    t.end_fill()
+
+    t.penup()
+    t.goto(-45, 110)
+    t.pendown()
+    t.begin_fill()
+    t.color("black")
+    t.circle(10)
+    t.end_fill()
+
+    t.penup()
+    t.goto(75, 110)
+    t.pendown()
+    t.begin_fill()
+    t.circle(10)
+    t.end_fill()
+
+    # Nose
+    t.penup()
+    t.goto(15, 70)
+    t.pendown()
+    t.begin_fill()
+    t.circle(10)
+    t.end_fill()
+
+    # Mouth
+    t.penup()
+    t.goto(-25, 50)
+    t.pendown()
+    t.right(90)
+    t.circle(20, 180)
+
+    t.penup()
+    t.goto(-25, 50)
+    t.pendown()
+    t.left(180)
+    t.circle(-20, 180)
+
+    # Cheeks
+    t.penup()
+    t.goto(-75, 55)
+    t.pendown()
+    t.begin_fill()
+    t.color("red")
+    t.circle(25)
+    t.end_fill()
+
+    t.penup()
+    t.goto(85, 55)
+    t.pendown()
+    t.begin_fill()
+    t.circle(25)
+    t.end_fill()
+
+    t.hideturtle()
+    t.done()
+
+if __name__ == "__main__":
+    draw_pikachu()
